13a13678686cae747b9c4ce5e8ebbaa2fc7494f8
sage2002 <gubai17092002@gmail.com>
2025-02-09 16:18:33 +0530

    bottombar, rightsidebar implemented

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs"
 import Link from "next/link";
 import Image from "next/image";
+import { dark } from "@clerk/themes";
 function Topbar() {
     return (
     <nav className="topbar">
@@ -31,6 +32,7 @@ function Topbar() {
 
             <OrganizationSwitcher 
                 appearance={{
+                    baseTheme: dark,
                     elements: {
                         organizationSwitcherTrigger:
                         "py-2 px-4"
