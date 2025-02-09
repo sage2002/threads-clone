7353a63538bc68cf5639c68ef9163b2ec063693f
sage2002 <gubai17092002@gmail.com>
2025-02-09 12:38:17 +0530

    Bottombar and leftsidebar alignment

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,36 +8,59 @@ function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
     return (
-    <section className="custom-scrollbar 
-        LeftSidebar">
+        <section className="custom-scrollbar
+        leftsidebar">
             <div className="flex w-full flex-1
             flex-col gap-6 px-6">
                 {sidebarLinks.map((link) => {
-                    const isActive = (pathname.includes
-                        (link.route) && link.route.length >
-                        1) || pathname === link.route;
+                   const isActive = (pathname.includes
+                    (link.route) && link.route.length >
+                    1) || pathname === link.route;
 
-                    return (
-                        <Link 
-                            href={link.route}
-                            key={link.label}
-                            className={`leftsidebar_link
-                            ${isActive && 'bg-primary-500'}`}
-                        >
-                            <Image 
-                                src={link.imgURL}
-                                alt={link.label}
-                                width={24}
-                                height={24}
-                            />
-                            <p className="text-light-1 
-                            max-lg:hidden">{link.label}</p>
-                        </Link>
-                    
-                    )}
+                return (
+                    <Link 
+                        href={link.route}
+                        key={link.label}
+                        className={`leftsidebar_link
+                        ${isActive && 'bg-primary-500'}`}
+                    >
+                        <Image 
+                            src={link.imgURL}
+                            alt={link.label}
+                            width={24}
+                            height={24}
+                        />
+                        <p className="text-light-1 
+                        max-lg:hidden">{link.label}</p>
+                    </Link>
+                
                 )}
+            )}
+        </div>
+
+        <div className="mt-10 px-6">
+            <SignedIn>
+                <SignOutButton redirectUrl="/sign-in">
+                    <div className="flex
+                    cusor-pointer gap-4 p-4">
+                        <Image
+                            src="/assets/logout.svg"
+                            alt="logout"
+                            width={24}
+                            height={24}
+                        />
+
+                        <p className="text-light-2
+                        max-lg:hidden">Logout</p>
+                    </div>
+
+                </SignOutButton>
+            </SignedIn>
+
+            
+
             </div>
-    </section>
+        </section>
     )
 }
 
